c16b8f4ece27807a530481a310fe7cc05aa663a3
Lily Copeland <lilykcopeland@gmail.com>
2020-01-28 10:28:58 -0800

    Updated Dice App with sender.tag

--- App2/ViewController.swift
+++ App2/ViewController.swift
@@ -17,46 +17,54 @@ class ViewController: UIViewController {
     
     //Variables
     var diceRoll = 0
+    var numberOfRolls = 0; 
 
     
     //Label
        @IBOutlet weak var messageLabel: UILabel!
+        @IBOutlet weak var messageLabel1: UILabel!
     
     //Buttons
     @IBAction func fourSidedPushed(_ sender: UIButton) {
         
-          diceRoll = Int.random(in: 1...4)
-    
-        messageLabel.text = "You rolled a 4 Sided Dice and got a " + String(diceRoll)
+        diceRoll = Int.random(in: 1...sender.tag)
+        messageLabel.text = "You rolled a \(sender.tag) Sided Dice and got a " + String(diceRoll)
+        
     }
     @IBAction func sixSidedPushed(_ sender: UIButton) {
 
-                diceRoll = Int.random(in: 1...6)
-                messageLabel.text = "You rolled a 6 Sided Dice and got a " + String(diceRoll)
+                diceRoll = Int.random(in: 1...sender.tag)
+                messageLabel.text = "You rolled a \(sender.tag) Sided Dice and got a " + String(diceRoll)
+                
     }
     
     @IBAction func eightSidedPushed(_ sender: UIButton) {
-            diceRoll = Int.random(in: 1...8)
-            messageLabel.text = "You rolled a 8 Sided Dice and got a " + String(diceRoll)
+            diceRoll = Int.random(in: 1...sender.tag)
+            messageLabel.text = "You rolled a \(sender.tag) Sided Dice and got a " + String(diceRoll)
+            
     }
     
     @IBAction func tenSidedPushed(_ sender: UIButton) {
-                    diceRoll = Int.random(in: 1...10)
-                      messageLabel.text = "You rolled a 10 Sided Dice and got a " + String(diceRoll)
+                diceRoll = Int.random(in: 1...sender.tag)
+                messageLabel.text = "You rolled a \(sender.tag) Sided Dice and got a " + String(diceRoll)
     }
+                    
     @IBAction func twelveSidedPushed(_ sender: UIButton) {
-         diceRoll = Int.random(in: 1...12)
-        messageLabel.text = "You rolled a 12 Sided Dice and got a " + String(diceRoll)
+         diceRoll = Int.random(in: 1...sender.tag)
+         messageLabel.text = "You rolled a \(sender.tag) Sided Dice and got a " + String(diceRoll)
+         
     }
         @IBAction func twentySidedPushed(_ sender: UIButton) {
-          diceRoll = Int.random(in: 1...20)
-         messageLabel.text = "You rolled a 20 Sided Dice and got a " + String(diceRoll)
+          diceRoll = Int.random(in: 1...sender.tag)
+          messageLabel.text = "You rolled a \(sender.tag) Sided Dice and got a " + String(diceRoll)
+          
     }
     
 
         @IBAction func clearButton(_ sender: UIButton) {
         messageLabel.text = " "
     }
+
     
     
     
